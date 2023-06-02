32b97ed3ef752e3b159e7e782b1ce0a5b87c49f4
Yaroslav <adolas666@gmail.com>
2023-06-02 18:29:36 +0300

    package files updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,22 @@
-const { defineConfig } = require("cypress");
-const faker = require("faker");
-const { clear } = require("./server/db");
-const { seed } = require("./server/db");
+const { defineConfig } = require('cypress');
+const faker = require('faker');
+const { clear } = require('./server/db');
+const { seed } = require('./server/db');
 const {
-  addMatchImageSnapshotPlugin,
+  addMatchImageSnapshotPlugin
 } = require('cypress-image-snapshot/plugin');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:1667/',
     setupNodeEvents(on, config) {
-      on("task", {
+      on('task', {
         generateUser() {
-          randomNumber = Math.ceil(Math.random(1000) * 1000);
+          const randomNumber = Math.ceil(Math.random(1000) * 1000);
           return {
             username: faker.name.firstName() + `${randomNumber}`,
-            email: 'test'+`${randomNumber}`+'@mail.com',
-            password: '12345Qwert!',
+            email: 'test' + `${randomNumber}` + '@mail.com',
+            password: '12345Qwert!'
           };
         },
         generateArticle() {
@@ -25,7 +25,7 @@ module.exports = defineConfig({
             description: faker.lorem.words(),
             body: faker.lorem.words(),
             tag: faker.lorem.word()
-          };;
+          };
         },
         'db:clear'() {
           clear();
@@ -39,6 +39,6 @@ module.exports = defineConfig({
         }
       });
       addMatchImageSnapshotPlugin(on, config);
-    },
-  },
+    }
+  }
 });
