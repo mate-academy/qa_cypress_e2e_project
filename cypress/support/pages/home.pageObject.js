f02ea7ab44bcc9199d14547aaadb21b281b1a701
iryna <irabeznoshchenko@gmail.com>
2023-06-19 19:20:01 +0300

    Conduit

--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -1,11 +1,13 @@
 import PageObject from '../PageObject';
-
 class HomePageObject extends PageObject {
   url = '/#/';
 
   get usernameLink() {
-    return cy.getByDataCy('username-link');
+    return cy.getByDataCy('usernameLink');
+  }
+
+  cheackArticlesList() {
+    cy.get('.article-preview')
+      .should('contain', 'No articles')
   }
 }
-
-export default HomePageObject;
