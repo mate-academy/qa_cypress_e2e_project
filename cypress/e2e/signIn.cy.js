c10db371f61280110392dc61c35cb8d59ccbfaca
Iia Hnatiuk <iyuschaaa@gmail.com>
2023-06-20 15:11:31 +0300

    IIA e2e signInPage

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -10,7 +10,7 @@ const homePage = new HomePageObject();
 describe('Sign In page', () => {
   let user;
 
-  before(() => {
+  beforeEach(() => {
     cy.task('db:clear');
     cy.task('generateUser').then(generateUser => {
       user = generateUser;
@@ -18,8 +18,8 @@ describe('Sign In page', () => {
   });
 
   it('should provide an ability to log in with existing credentials', () => {
-    signInPage.visit();
     cy.register(user.email, user.username, user.password);
+    signInPage.visit();
 
     signInPage.emailField
       .type(user.email);
@@ -33,6 +33,16 @@ describe('Sign In page', () => {
   });
 
   it('should not provide an ability to log in with wrong credentials', () => {
+    cy.register(user.email, user.username, user.password);
+    signInPage.visit();
 
+    signInPage.emailField
+      .type(`iia${user.email}`);
+    signInPage.passwordField
+      .type(user.password);
+    signInPage.signInBtn
+      .click();
+
+    signInPage.assertFailedSignIn();
   });
 });
