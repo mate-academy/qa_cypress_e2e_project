212eb15f8674fe6ecd9e4052298c3ce1466f1825
Yuliia Vereshko <pyliatko@gmail.com>
2023-06-20 10:54:40 +0200

    Sign Up page

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -28,8 +28,8 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand();
 
-Cypress.Commands.add('getByDataCy', (selector) => {
-  cy.get(`[data-cy="${selector}"]`);
+Cypress.Commands.add('getByDataQA', (selector) => {
+  cy.get(`[data-qa="${selector}"]`);
 });
 
 Cypress.Commands.add('register', (email = 'riot@qa.team', username = 'riot', password = '12345Qwert!') => {
@@ -37,5 +37,50 @@ Cypress.Commands.add('register', (email = 'riot@qa.team', username = 'riot', pas
     email,
     username,
     password
+  });
+});
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder="${placeholder}"]`);
+});
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.request('POST', '/users/login', {
+    user: {
+      email,
+      password
+    }
+  }).then(response => {
+    const user = {
+      bio: response.body.user.bio,
+      effectiveImage: 'https://static.productionready.io/images/smiley-cyrus.jpg',
+      email: response.body.user.email,
+      image: response.body.user.image,
+      token: response.body.user.token,
+      username: response.body.user.username
+    };
+    window.localStorage.setItem('user', JSON.stringify(user));
+    cy.setCookie('auth', response.body.user.token);
+  });
+});
+
+Cypress.Commands.add('createArticle', (title, description, body) => {
+  cy.getCookie('auth').then((token) => {
+    const authToken = token.value;
+    cy.request({
+      method: 'POST',
+      url: 'api/articles',
+      body: {
+        article: {
+          title,
+          description,
+          body,
+          tagList: []
+        }
+      },
+      headers: {
+        Authorization: `Token ${authToken}`
+      }
+    });
   });
 });
