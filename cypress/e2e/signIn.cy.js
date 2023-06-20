6418f6144741e726e042a9a51d869a66e80e66a0
Bodzian Mykhailo <bodzianmykhailo@gmail.com>
2023-06-20 16:36:05 +0300

    Article SignIn SignUp Solutions

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,38 +1,62 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-import SignInPageObject from '../support/pages/signIn.pageObject';
-import HomePageObject from '../support/pages/home.pageObject';
+import SignInPageObject from '../support/pages/signIn.pageObject'
+import HomePageObject from '../support/pages/home.pageObject'
 
-const signInPage = new SignInPageObject();
-const homePage = new HomePageObject();
+const signInPage = new SignInPageObject()
+const homePage = new HomePageObject()
 
 describe('Sign In page', () => {
-  let user;
+  let user
 
-  before(() => {
-    cy.task('db:clear');
-    cy.task('generateUser').then(generateUser => {
-      user = generateUser;
-    });
-  });
+  beforeEach(() => {
+    cy.task('db:clear')
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser
+    })
+  })
 
   it('should provide an ability to log in with existing credentials', () => {
-    signInPage.visit();
-    cy.register(user.email, user.username, user.password);
+    signInPage.visit()
+    cy.register(user.email, user.username, user.password)
+    cy.intercept('POST', '/users/login').as('login')
 
-    signInPage.emailField
-      .type(user.email);
-    signInPage.passwordField
-      .type(user.password);
-    signInPage.signInBtn
-      .click();
+    signInPage.emailField.type(user.email)
+    signInPage.passwordField.type(user.password)
+    signInPage.signInBtn.click()
 
-    homePage.usernameLink
-      .should('contain', user.username);
-  });
+    cy.wait('@login')
+    homePage.usernameLink.should('contain', user.username)
+  })
 
-  it('should not provide an ability to log in with wrong credentials', () => {
+  it('should not provide an ability to log in with wrong Password', () => {
+    const password = 'blablabla'
+    const unsuccessLoginTitle = 'Login failed!'
+    const unsuccessLoginText = 'Invalid user credentials.'
+    signInPage.visit()
+    cy.register(user.email, user.username, user.password)
 
-  });
-});
+    signInPage.emailField.type(user.email)
+    signInPage.passwordField.type(password)
+    signInPage.signInBtn.click()
+
+    cy.get('.swal-title').should('have.text', unsuccessLoginTitle)
+    cy.get('.swal-text').should('have.text', unsuccessLoginText)
+  })
+
+  it('should not provide an ability to log in with wrong Email', () => {
+    const email = 'blablabla'
+    const unsuccessLoginTitle = 'Login failed!'
+    const unsuccessLoginText = 'Email must be a valid email.'
+    signInPage.visit()
+    cy.register(user.email, user.username, user.password)
+
+    signInPage.emailField.type(email)
+    signInPage.passwordField.type(user.password)
+    signInPage.signInBtn.click()
+
+    cy.get('.swal-title').should('have.text', unsuccessLoginTitle)
+    cy.get('.swal-text').should('have.text', unsuccessLoginText)
+  })
+})
--- src/vue/routes/ArticleEdit.vue
+++ src/vue/routes/ArticleEdit.vue
@@ -10,6 +10,7 @@
                 <input
                   type="text"
                   class="form-control form-control-lg"
+                  data-cy="article-title-field"
                   v-model="article.title"
                   placeholder="Article Title"
                 />
@@ -18,6 +19,7 @@
                 <input
                   type="text"
                   class="form-control"
+                  data-cy="article-description-field"
                   v-model="article.description"
                   placeholder="What's this article about?"
                 />
@@ -25,6 +27,7 @@
               <fieldset class="form-group">
                 <textarea
                   class="form-control"
+                  data-cy="article-body-field"
                   rows="8"
                   v-model="article.body"
                   placeholder="Write your article (in markdown)"
@@ -35,6 +38,7 @@
                 <vue-tags-input
                   placeholder="Enter tags"
                   class="form-control"
+                  data-cy="article-tags-field"
                   v-model="tag"
                   :tags="tags"
                   @tags-changed="newTags => tags = newTags"
@@ -44,6 +48,7 @@
             <button
               :disabled="publishing_article"
               class="btn btn-lg pull-xs-right btn-primary"
+              data-cy="publish-article-btn"
               type="submit"
             >
               Publish Article
