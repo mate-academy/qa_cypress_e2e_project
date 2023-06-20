8be176d07efccc5d28d75404a8548021ba35eeb3
Oleksii Shypovalov <asipovalov9@gmail.com>
2023-06-20 14:23:29 +0300

    SignUp Page

--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -11,6 +11,11 @@ class HomePageObject extends PageObject {
     cy.get('.article-preview')
       .should('contain', 'No articles')
   }
+
+  checkRegistration() {
+    cy.get('.swal-text')
+      .should('contain', 'Your registration was successful!');
+  }
 }
 
 export default HomePageObject;
