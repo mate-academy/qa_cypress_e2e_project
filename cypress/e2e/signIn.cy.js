dd162ed8b6011ff1a3cad8e9156961db6b1030f5
Denys Shyshlakov <444.spam@gmail.com>
2023-06-21 10:21:09 +0300

    Sign in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -10,17 +10,17 @@ const homePage = new HomePageObject();
 describe('Sign In page', () => {
   let user;
 
-  before(() => {
+  beforeEach(() => {
     cy.task('db:clear');
+    signInPage.visit();
     cy.task('generateUser').then(generateUser => {
       user = generateUser;
     });
   });
 
   it('should provide an ability to log in with existing credentials', () => {
-    signInPage.visit();
+    
     cy.register(user.email, user.username, user.password);
-
     signInPage.emailField
       .type(user.email);
     signInPage.passwordField
@@ -32,7 +32,24 @@ describe('Sign In page', () => {
       .should('contain', user.username);
   });
 
-  it('should not provide an ability to log in with wrong credentials', () => {
-
+  it('should not provide an ability to log in with not registered email', () => {
+    cy.register(user.email, user.username, user.password);
+    signInPage.emailField
+      .type('random@sht.email');
+    signInPage.passwordField
+      .type(user.password);
+    signInPage.signInBtn
+      .click();
+    cy.contains('.swal-title','Login failed');
+  });
+  it('should not provide an ability to log in with wrong password', () => {
+    cy.register(user.email, user.username, user.password);
+    signInPage.emailField
+      .type(user.email);
+    signInPage.passwordField
+      .type('wrongpassword');
+    signInPage.signInBtn
+      .click();
+    cy.contains('.swal-title','Login failed');
   });
 });
