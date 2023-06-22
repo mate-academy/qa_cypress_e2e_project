72e866310a822fd3ad423f604905fd6f12b5357a
YuryTatarko <hatemaders@gmail.com>
2023-06-22 16:10:25 +0300

    Project code

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,25 +1,37 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
-
 import SignInPageObject from '../support/pages/signIn.pageObject';
 import HomePageObject from '../support/pages/home.pageObject';
-
 const signInPage = new SignInPageObject();
 const homePage = new HomePageObject();
-
 describe('Sign In page', () => {
   let user;
 
   before(() => {
-    cy.task('db:clear');
     cy.task('generateUser').then(generateUser => {
       user = generateUser;
     });
   });
 
+  beforeEach(() => {
+    cy.task('db:clear');
+  });
+
   it('should provide an ability to log in with existing credentials', () => {
     signInPage.visit();
     cy.register(user.email, user.username, user.password);
+    signInPage.emailField
+      .type(user.email);
+    signInPage.passwordField
+      .type(user.password);
+    signInPage.signInBtn
+      .click();
+    homePage.usernameLink
+      .should('contain', user.username);
+  });
+
+  it('should not provide an ability to log in with wrong credentials', () => {
+    signInPage.visit();
 
     signInPage.emailField
       .type(user.email);
@@ -28,11 +40,8 @@ describe('Sign In page', () => {
     signInPage.signInBtn
       .click();
 
-    homePage.usernameLink
-      .should('contain', user.username);
-  });
-
-  it('should not provide an ability to log in with wrong credentials', () => {
+      cy.get('.swal-modal')
+        .should('contain', 'Login failed')
 
   });
 });
