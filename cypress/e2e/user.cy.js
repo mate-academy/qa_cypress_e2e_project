7e2ccdd547a6dff2cbd2d076b32d39a14ceb768b
Your GitHub Username <Your GitHub Email>
2023-06-22 16:47:18 +0300

    Added tests for following/unfollowing the user and fixed missing lines

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,12 +1,39 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-describe('User', () => {
-  before(() => {
+import UserProfilePageObject from '../support/pages/userProfile.pageObject';
+const userProfilePage = new UserProfilePageObject();
 
+describe('User', () => {
+  let user; 
+  let newUser;
+
+  beforeEach(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then(generateUser => {
+      user = generateUser;
+    });
+    cy.task('generateUser').then(generateUser => {
+      newUser = generateUser;
+    });
   });
 
   it.skip('should be able to follow the another user', () => {
+    cy.register(user.email, user.username, user.password);
+    cy.register(newUser.email, newUser.username, newUser.password);
+    cy.login(newUser.email, newUser.password);
+    userProfilePage.visitUserPage(user.username);
+    userProfilePage.clickFollowBtn();
+    userProfilePage.checkFollowing();
+  });
 
+  it.skip('should be able to unfollow the another user', () => {
+    cy.register(user.email, user.username, user.password);
+    cy.register(newUser.email, newUser.username, newUser.password);
+    cy.login(newUser.email, newUser.password);
+    userProfilePage.visitUserPage(user.username);
+    userProfilePage.clickFollowBtn();
+    userProfilePage.clickUnfollowBtn();
+    userProfilePage.checkUnfollowing();
   });
 });
