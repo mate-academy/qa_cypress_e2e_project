76a03e0cb054cd001911eb36b19c29a0b0ac8805
Yelyzaveta Nikitina <lnikitina96@gmail.com>
2023-08-04 15:07:03 +0300

    e2e tests for article, sign in and sign up

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,24 +1,65 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Article', () => {
-  before(() => {
+import CreateArticlePageObject from '../support/pages/createArticle.pageObject';
+import EditArticlePageObject from '../support/pages/editArticlePageObject';
+import ArticlePageObject from '../support/pages/article.pageObject';
 
-  });
+const createArticlePage = new CreateArticlePageObject();
+const editArticlePage = new EditArticlePageObject();
+const articlePage = new ArticlePageObject();
+
+describe('Article', () => {
+  let user;
+  let article;
 
   beforeEach(() => {
     cy.task('db:clear');
+
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
   it('should be created using New Article form', () => {
+    cy.login(user.email, user.username, user.password);
+    createArticlePage.visit();
 
+    createArticlePage.typeTitle(article.title);
+    createArticlePage.typeDescription(article.description);
+    createArticlePage.typeBody(article.body);
+    createArticlePage.typeTags(article.tag);
+    createArticlePage.clickOnPublishBtn();
+
+    cy.get('h1').should('contain', article.title);
+    cy.url().should('include', article.title);
   });
 
-  it('should be edited using Edit button', () => {
+  it.only('should be edited using Edit button', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article);
 
+    articlePage.clickOnEditBtn();
+    editArticlePage.editTitle('[Edited]');
+    editArticlePage.editDescription(article.description + '[Edited]');
+    editArticlePage.editBody(article.body + '[Edited]');
+    editArticlePage.editTags(article.tag + '[Edited]');
+    editArticlePage.clickOnPublishBtn();
+
+    cy.get('h1').should('contain', article.title + '[Edited]');
   });
 
   it('should be deleted using Delete button', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article);
 
+    articlePage.clickOnDeleteBtn();
+
+    cy.get('h1').should('contain', 'conduit');
+    cy.get('p').should('contain', 'A place to share your knowledge.');
   });
 });
---
+++ cypress/support/pages/editArticle.pageObject.js
@@ -0,0 +1,28 @@
+import PageObject from '../PageObject';
+
+class EditArticlePageObject extends PageObject {
+  url = '/#/editor';
+
+  editTitle(title) {
+    cy.findByPlaceholder('Article Title').type(title);
+  }
+
+  editDescription(description) {
+    // eslint-disable-next-line max-len
+    cy.findByPlaceholder('What\'s this article about?').clear().type(description);
+  }
+
+  editBody(body) {
+    cy.findByPlaceholder('Write your article (in markdown)').clear().type(body);
+  }
+
+  editTags(tag) {
+    cy.findByPlaceholder('Enter tags').clear().type(tag);
+  }
+
+  clickOnPublishBtn() {
+    cy.contains('button', 'Publish Article').click();
+  }
+}
+
+export default EditArticlePageObject;
