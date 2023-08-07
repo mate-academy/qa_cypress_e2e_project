eaf4bc97930a9bd86004f5bd7474547e25af0161
Oleksandr Melnyk <snchkmelnyk@gmail.com>
2023-08-07 09:42:14 +0300

    user settings testing

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,64 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
+import HomePageObject from '../support/pages/home.pageObject';
+import SignInPageObject from '../support/pages/signIn.pageObject';
+import SettingsUserPageObject from '../support/pages/settingsUser.pageObject';
+
+const signInPage = new SignInPageObject();
+const homePage = new HomePageObject();
+const settingsUser = new SettingsUserPageObject();
+
+let user;
+
 describe('Settings page', () => {
-  before(() => {
-
-  });
-
   beforeEach(() => {
-
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
+
+  const signInAndVisitSettings = () => {
+    signInPage.visit();
+    cy.register(user.email, user.username, user.password);
+    signInPage.typeEmail(user.email);
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+  };
 
   it('should provide an ability to update username', () => {
-
+    signInAndVisitSettings();
+    settingsUser.editUsername();
+    settingsUser.assertSuccesfulEdit();
+    homePage.homePageRedirect();
+    homePage.assertHeaderContainUpdateUsername();
   });
 
   it('should provide an ability to update bio', () => {
-
+    signInAndVisitSettings();
+    settingsUser.editBio();
+    settingsUser.assertSuccesfulEdit();
+    homePage.homePageRedirect();
+    homePage.profileRedirect();
+    settingsUser.assertBioUser();
   });
 
   it('should provide an ability to update an email', () => {
-
+    signInAndVisitSettings();
+    settingsUser.editEmail();
+    settingsUser.assertSuccesfulEdit();
   });
 
   it('should provide an ability to update password', () => {
-
+    signInAndVisitSettings();
+    settingsUser.editPassword();
+    settingsUser.assertSuccesfulEdit();
   });
 
   it('should provide an ability to log out', () => {
-
+    signInAndVisitSettings();
+    settingsUser.logoutUser();
+    settingsUser.assertUserIsLogedout();
   });
 });
---
+++ cypress/support/pages/settingsUser.pageObject.js
@@ -0,0 +1,50 @@
+import PageObject from '../PageObject';
+
+class SettingsUserPageObject extends PageObject {
+  url = '/#/settings';
+
+  editUsername(username) {
+    cy.get(':nth-child(3) > .nav-link').click();
+    cy.findByPlaceholder('Your username').clear().type('JohnSilver');
+    cy.get('form > :nth-child(1) > .btn').click();
+  }
+
+  assertSuccesfulEdit() {
+    cy.get('.swal-title').should('contain', 'Update successful!');
+    cy.get('.swal-button').click();
+  }
+
+  editBio() {
+    cy.get(':nth-child(3) > .nav-link').click();
+    cy.findByPlaceholder('Short bio about you').type('The coollest guy!');
+    cy.get('form > :nth-child(1) > .btn').click();
+  }
+
+  editEmail(email) {
+    cy.get(':nth-child(3) > .nav-link').click();
+    cy.findByPlaceholder('Email').type('adamsmasher@mail.com');
+    cy.get('form > :nth-child(1) > .btn').click();
+  }
+
+  editPassword(password) {
+    cy.get(':nth-child(3) > .nav-link').click();
+    cy.findByPlaceholder('Password').type('PlazmaSh0k!');
+    cy.get('form > :nth-child(1) > .btn').click();
+  }
+
+  assertUserIsLogedout() {
+    cy.get(':nth-child(2) > .nav-link').should('contain', 'Sign in');
+  }
+
+  assertBioUser() {
+    cy.get('p').should('contain', 'The coollest guy!');
+  }
+
+  logoutUser() {
+    cy.get(':nth-child(3) > .nav-link').click();
+    cy.get('.text-xs-center').should('contain', 'Your Settings');
+    cy.get('.btn-outline-danger').click();
+  }
+}
+
+export default SettingsUserPageObject;
