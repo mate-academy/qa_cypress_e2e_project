a3c8c933298677cb9c91aeb428fd40193729b5a3
Oleksandr Melnyk <snchkmelnyk@gmail.com>
2023-08-07 09:20:35 +0300

    user follow/unfollow testing

---
+++ cypress/support/pages/followUnfollow.pageObject.js
@@ -0,0 +1,19 @@
+import PageObject from '../PageObject';
+
+class FollowUnfollowPageObject extends PageObject {
+  followUser() {
+    cy.get('.feed-toggle > .nav > :nth-child(1) > .nav-link').click();
+
+    cy.get('.article-preview').click();
+    // eslint-disable-next-line max-len
+    cy.get('.article-actions > .article-meta > :nth-child(3) > .btn-outline-secondary > :nth-child(3)').click();
+  }
+
+  unfollowUser(username) {
+    cy.visit('#/my-feed');
+    cy.get('.article-preview').contains(username).click();
+    cy.get('.col-xs-12 > div > .btn').contains('Unfollow').click();
+  }
+}
+
+export default FollowUnfollowPageObject;
