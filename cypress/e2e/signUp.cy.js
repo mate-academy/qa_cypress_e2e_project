111c1f03a2507cbf486438631229cbc1da554b8b
Dmytro Ieromin <dmytroyerominfcdk@gmail.com>
2023-08-08 15:07:07 +0300

    e2e testing practice

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,12 +1,57 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Sign Up page', () => {
-  before(() => {
+import SignUpPageObject from '../support/pages/signUp.pageObject';
+import HomePageObject from '../support/pages/home.pageObject';
 
+const signUpPage = new SignUpPageObject();
+const homePage = new HomePageObject();
+
+describe('Sign Up page', () => {
+  let user;
+
+  beforeEach(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
-  it('should ...', () => {
+  const userRegister = (username, email, password) => {
+    signUpPage.visit();
+    signUpPage.typeUsername(username);
+    signUpPage.typeEmail(email);
+    signUpPage.typePassword(password);
+    signUpPage.clickSignUpBtn();
+  };
 
+  it('should provide an ability to sign up', () => {
+    userRegister(user.username, user.email, user.password);
+    signUpPage.assertSuccessfulRegistration();
+    homePage.assertHeaderContainUsername(user.username);
+  });
+
+  // eslint-disable-next-line max-len
+  it('should not provide an ability to sign up with one space only in password field', () => {
+    userRegister(user.username, user.email, ' ');
+
+    signUpPage.assertEmptyPassword();
+    signUpPage.assertFailedRegistartion();
+  });
+
+  // eslint-disable-next-line max-len
+  it('should not provide an ability to sign up with one space only in email field', () => {
+    userRegister(user.username, ' ', user.password);
+
+    signUpPage.assertEmptyEmail();
+    signUpPage.assertFailedRegistartion();
+  });
+
+  // eslint-disable-next-line max-len
+  it('should not provide an ability to sign up with one space only in username field', () => {
+    userRegister(' ', user.email, user.password);
+
+    signUpPage.assertEmptyUsername();
+    signUpPage.assertFailedRegistartion();
   });
 });
--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,12 +1,37 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('User', () => {
-  before(() => {
+import UserPageObject from '../support/pages/user.pageObject';
 
+const userPage = new UserPageObject();
+
+describe('User', () => {
+  let user;
+  let follow;
+
+  beforeEach(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateFollow').then((generateFollow) => {
+      follow = generateFollow;
+    });
   });
 
   it.skip('should be able to follow the another user', () => {
+    cy.register(user.email, user.username, user.password);
+    cy.followRegister(follow.email, follow.username, follow.password);
+    userPage.visitUserPage(user.username);
+    userPage.clickFollowBtn(user.username);
+  });
 
+  it('should be able to unfollow the another user', () => {
+    cy.register(user.email, user.username, user.password);
+    cy.followRegister(follow.email, follow.username, follow.password);
+    userPage.visitUserPage(user.username);
+    userPage.clickFollowBtn(user.username);
+    userPage.clickUnfollowBtn();
+    userPage.assertUnfollowBtn();
   });
 });
