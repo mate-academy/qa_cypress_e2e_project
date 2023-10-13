f6abf4156ab35cee97b08b83b7653ed670947fce
Svitlana Zai <svetkaqq1991@gmail.com>
2023-10-13 03:35:11 +0300

    profile page tests

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,12 +1,36 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
+import UserPageObject from '../support/pages/user.pageObject';
+
+const userPage = new UserPageObject();
 
 describe('User', () => {
-  before(() => {
+  let user;
+  let user2;
 
+  beforeEach(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+      cy.register(user.username, user.email, user.password);
+    });
+    cy.task('generateUser').then((generateUser) => {
+      user2 = generateUser;
+      cy.login(user2.username, user2.email, user2.password);
+    });
   });
 
-  it.skip('should be able to follow the another user', () => {
+  it('should provide an ability to follow user', () => {
+    userPage.visitFollowedUserPage(user.username);
+    userPage.assertFollowBtnExist();
+    userPage.clickFollowUserBtn();
+    userPage.assertUnfollowBtnExist();
+  });
 
+  it('should provide an ability to unfollow the user', () => {
+    userPage.visitFollowedUserPage(user.username);
+    userPage.clickFollowUserBtn();
+    userPage.clickUnfollowUserBtn();
+    userPage.assertFollowBtnExist();
   });
 });
