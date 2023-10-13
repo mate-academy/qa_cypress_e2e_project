fcedf53ade7dd274a07b6e9a427a54007b74d6ea
Svitlana Zai <svetkaqq1991@gmail.com>
2023-10-13 15:13:56 +0300

    add new commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,17 +25,67 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+import SignInPageObject from './pages/signIn.pageObject';
 
 addMatchImageSnapshotCommand();
 
-Cypress.Commands.add('getByDataCy', (selector) => {
-  cy.get(`[data-cy="${selector}"]`);
+Cypress.Commands.add('getByDataQa', (selector) => {
+  cy.get(`[data-qa="${selector}"]`);
 });
 
-Cypress.Commands.add('register', (email = 'riot@qa.team', username = 'riot', password = '12345Qwert!') => {
+Cypress.Commands.overwrite('visit', (originalFn, url, Option) => {
+  originalFn('/#' + url);
+});
+
+Cypress.Commands.add('register', (username, email, password) => {
   cy.request('POST', '/users', {
-    email,
     username,
+    email,
     password
+  });
+});
+
+Cypress.Commands.add('loginSignInPage', (email, password) => {
+  const signInPage = new SignInPageObject();
+
+  signInPage.visit();
+  signInPage.typeEmail(email);
+  signInPage.typePassword(password);
+  signInPage.clickSignInBtn();
+});
+
+Cypress.Commands.add('login', (username, email, password) => {
+  cy.request('POST', '/users', {
+    username,
+    email,
+    password
+  }).then((response) => {
+    const user = {
+      id: response.body.user.id,
+      username: response.body.user.username,
+      email: response.body.user.email,
+      bio: response.body.user.bio,
+      image: response.body.user.image,
+      token: response.body.user.token
+    };
+    window.localStorage.setItem('user', JSON.stringify(user));
+    cy.setCookie('drash_sess', response.body.user.token);
+  });
+});
+
+Cypress.Commands.add('getRegisterUser', () => {
+  const registerUser = JSON.parse(window.localStorage.getItem('user'));
+  return registerUser;
+});
+
+Cypress.Commands.add('createArticle', (id, body, description, tag, title) => {
+  cy.request('POST', 'articles', {
+    article: {
+      author_id: id,
+      body,
+      description,
+      tags: tag,
+      title
+    }
   });
 });
