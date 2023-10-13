98b7d5b5b490f21d8ca78714fe40afb6e8b1f109
Svitlana Zai <svetkaqq1991@gmail.com>
2023-10-13 15:32:24 +0300

    add solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -4,13 +4,16 @@
 import ArticlePage from '../support/pages/article.pageObject';
 import NewArticlePage from '../support/pages/newArticle.pageObject';
 import ProfilePage from '../support/pages/profile.pageObject';
+import HomePage from '../support/pages/home.pageObject';
 
 const newArticlePage = new NewArticlePage();
 const profilePage = new ProfilePage();
 const articlePage = new ArticlePage();
+const homePage = new HomePage();
 
 describe('Article', () => {
   let user;
+  let user2;
   let article;
   let newArticle;
 
@@ -22,35 +25,51 @@ describe('Article', () => {
 
   beforeEach(() => {
     cy.task('db:clear');
+
     cy.task('generateUser').then((generateUser) => {
       user = generateUser;
-      cy.login(user.username, user.email, user.password);
+      cy.register(user.username, user.email, user.password);
     });
+
+    cy.task('generateUser').then((generateUser) => {
+      user2 = generateUser;
+      cy.login(user2.username, user2.email, user2.password);
+    });
+
     cy.task('generateArticle').then((generateArticle) => {
       article = generateArticle;
-    });
-    cy.getRegisterUser().then((user) => {
-      cy.createArticle(user.id, article.body,
-        article.description, article.tags, article.title);
     });
   });
 
   it('should be created using New Article form', () => {
-    newArticlePage.visit();
+    cy.loginSignInPage(user.email, user.password);
+    homePage.clickNewArticleForm();
     newArticlePage.typeTitle(article.title);
     newArticlePage.typeDescription(article.description);
     newArticlePage.typeBody(article.body);
     newArticlePage.typeTags(article.tag);
     newArticlePage.clickPublishArticleBtn();
 
-    articlePage.assertArticleTitleUrl(article.title);
     articlePage.assertTitle(article.title);
     articlePage.assertBody(article.body);
     articlePage.assertAuthorName(user.username);
+
+    homePage.clickUsernameLink();
+    profilePage.clickOnMyArticleTab();
+    profilePage.assertArticleInMyArticles(article.title);
   });
 
   it('should be edited using Edit button', () => {
-    articlePage.visit(article.title);
+    cy.getRegisterUser().then((user2) => {
+      cy.createArticle(user2.id, article.body,
+        article.description, article.tags, article.title);
+    });
+    cy.loginSignInPage(user2.email, user2.password);
+
+    homePage.clickUsernameLink();
+    profilePage.clickOnMyArticleTab();
+    profilePage.visitArticlePage(article.title);
+
     articlePage.clickEditArticleBtn();
     newArticlePage.editTitle(newArticle.title);
     newArticlePage.editDescription(newArticle.description);
@@ -58,25 +77,23 @@ describe('Article', () => {
     newArticlePage.editTags(newArticle.tag);
     newArticlePage.clickPublishArticleBtn();
 
-    articlePage.assertArticleTitleUrl(newArticle.title);
     articlePage.assertTitle(newArticle.title);
     articlePage.assertBody(newArticle.body);
   });
 
   it('should be deleted using Delete button ', () => {
-    profilePage.visit(user.username);
-    profilePage.clickOnMyArticleTab();
-    profilePage.assertArticleInMyArticles(article.title);
-    profilePage.visitArticlePage();
-    articlePage.clickDeleteArticleBtn();
-    profilePage.visit(user.username);
-    profilePage.assertArticleDeleted();
-  });
+    cy.getRegisterUser().then((user2) => {
+      cy.createArticle(user2.id, article.body,
+        article.description, article.tags, article.title);
+    });
+    cy.loginSignInPage(user2.email, user2.password);
 
-  it('should be deleted using Delete button', () => {
-    articlePage.visit(article.title);
+    homePage.clickUsernameLink();
+    profilePage.clickOnMyArticleTab();
+    profilePage.visitArticlePage(article.title);
+
     articlePage.clickDeleteArticleBtn();
-    profilePage.visit(user.username);
+    profilePage.visit(user2.username);
     profilePage.assertArticleDeleted();
   });
 });
