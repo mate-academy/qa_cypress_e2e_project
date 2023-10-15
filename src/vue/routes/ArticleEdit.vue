168696535823fd0340dbf8a0ec5b6f30fbf6df16
Olha <ogvozdeckas@gmail.com>
2023-10-15 22:15:54 +0200

    Project

--- src/vue/routes/ArticleEdit.vue
+++ src/vue/routes/ArticleEdit.vue
@@ -12,6 +12,7 @@
                   class="form-control form-control-lg"
                   v-model="article.title"
                   placeholder="Article Title"
+                  data-qa="article-title"
                 />
               </fieldset>
               <fieldset class="form-group">
@@ -20,6 +21,7 @@
                   class="form-control"
                   v-model="article.description"
                   placeholder="What's this article about?"
+                  data-qa="article-description"
                 />
               </fieldset>
               <fieldset class="form-group">
@@ -28,10 +30,11 @@
                   rows="8"
                   v-model="article.body"
                   placeholder="Write your article (in markdown)"
+                  data-qa="article-body"
                 >
                 </textarea>
               </fieldset>
-              <fieldset class="form-group">
+              <fieldset class="form-group" data-qa="article-tags">
                 <vue-tags-input
                   placeholder="Enter tags"
                   class="form-control"
@@ -45,6 +48,7 @@
               :disabled="publishing_article"
               class="btn btn-lg pull-xs-right btn-primary"
               type="submit"
+              data-qa="publish-article"
             >
               Publish Article
             </button>
