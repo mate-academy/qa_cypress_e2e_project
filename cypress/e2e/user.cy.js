f8806b598a489f7a89dffb6fd7b119462a46039a
Serhii Oliinyk <serhiioliinykqa@gmail.com>
2023-11-29 17:12:28 +0200

    Practice

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,12 +1,40 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('User', () => {
-  before(() => {
+import SignUpPageObject from '../support/pages/signUp.pageObject';
+import HomePageObject from '../support/pages/home.pageObject';
 
+const signUpPage = new SignUpPageObject();
+const homePage = new HomePageObject();
+
+describe('User', () => {
+  let user;
+  let article;
+
+  beforeEach(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it.skip('should be able to follow the another user', () => {
-
+  it('should be able to follow the another user', () => {
+    cy.createArticle(article.title, article.description,
+      article.body, article.tag).then((response) => {
+      cy.log(response);
+      const title = response.body.article.title;
+      signUpPage.visit();
+      signUpPage.typeuserName(user.username);
+      signUpPage.typeuserEmail(user.email);
+      signUpPage.typePassword(user.password);
+      signUpPage.clickOnSignUpBtn();
+      cy.get('.swal-button.swal-button--confirm').click();
+      cy.visit(`#/articles/${title}`);
+      homePage.followUser.eq(1).click();
+      homePage.followUser.eq(1).should('include.text', 'Unfollow');
+    });
   });
 });
