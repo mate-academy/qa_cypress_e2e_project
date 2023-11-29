565c8b79ef8e2155037410b0f1e76f764664e08d
Nataliya Hnativ <natkahnativ@gmail.com>
2023-11-29 16:27:55 +0200

    Cypress Project

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,5 +1,6 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
+const faker = require('faker');
 
 import SignInPageObject from '../support/pages/signIn.pageObject';
 import HomePageObject from '../support/pages/home.pageObject';
@@ -11,24 +12,55 @@ describe('Sign In page', () => {
   let user;
 
   before(() => {
-    cy.task('db:clear');
     cy.task('generateUser').then((generateUser) => {
       user = generateUser;
     });
   });
 
-  it('should provide an ability to log in with existing credentials', () => {
-    signInPage.visit();
-    cy.register(user.email, user.username, user.password);
+  beforeEach(() => {
+    cy.task('db:clear');
+  });
 
+  it('should provide an ability to log in with existing credentials', () => {
+    cy.register(user.username, user.email, user.password);
+    signInPage.visit();
     signInPage.typeEmail(user.email);
     signInPage.typePassword(user.password);
     signInPage.clickSignInBtn();
-
     homePage.assertHeaderContainUsername(user.username);
   });
 
-  it('should not provide an ability to log in with wrong credentials', () => {
+  it('should not provide an ability to log in with wrong email', () => {
+    cy.register(user.username, user.email, user.password);
+    signInPage.visit();
+    signInPage.typeEmail(user.newEmail);
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+    signInPage.assertLoginFailed();
+  });
 
+  it('should not provide an ability to log in with wrong password', () => {
+    cy.register(user.username, user.email, user.password);
+    signInPage.visit();
+    signInPage.typeEmail(user.email);
+    signInPage.typePassword(user.newPassword);
+    signInPage.clickSignInBtn();
+    signInPage.assertLoginFailed();
+  });
+
+  it('should not provide an ability to log in with empty email', () => {
+    cy.register(user.username, user.email, user.password);
+    signInPage.visit();
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+    signInPage.assertLoginEmptyEmail();
+  });
+
+  it('should not provide an ability to log in with empty password', () => {
+    cy.register(user.username, user.email, user.password);
+    signInPage.visit();
+    signInPage.typeEmail(user.email);
+    signInPage.clickSignInBtn();
+    signInPage.assertLoginEmptyPassword();
   });
 });
---
+++ cypress/support/pages/profile.pageObject.js
@@ -0,0 +1,14 @@
+import PageObject from '../PageObject';
+
+class ProfilePageObject extends PageObject {
+
+  get profileInfo() {
+    return cy.getByDataQa('profile-info');
+  }
+
+  assertProfileBio(bio) {
+    this.profileInfo.should('contain', bio);
+  }
+}
+
+export default ProfilePageObject;
