1d0bee67129d2abc0f07ea4672c5c7dedda6cd05
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-30 07:22:10 +0200

    PageObject.js

--- cypress/support/PageObject.js
+++ cypress/support/PageObject.js
@@ -1,4 +1,3 @@
-// PageObject.js
 class PageObject {
   visit(url) {
     cy.visit(url || this.url);
@@ -6,39 +5,3 @@ class PageObject {
 }
 
 export default PageObject;
-
-// signIn.cy.js
-/// <reference types='cypress' />
-/// <reference types='../support' />
-
-import SignInPageObject from '../support/pages/signIn.pageObject';
-import HomePageObject from '../support/pages/home.pageObject';
-
-const signInPage = new SignInPageObject();
-const homePage = new HomePageObject();
-
-describe('Sign In page', () => {
-  let user;
-
-  before(() => {
-    cy.task('db:clear');
-    cy.task('generateUser').then((generatedUser) => {
-      user = generatedUser;
-    });
-  });
-
-  it('should provide an ability to log in with existing credentials', () => {
-    signInPage.visit();
-    cy.register(user.email, user.username, user.password);
-
-    signInPage.typeEmail(user.email);
-    signInPage.typePassword(user.password);
-    signInPage.clickSignInBtn();
-
-    homePage.assertHeaderContainUsername(user.username);
-  });
-
-  it('should not provide an ability to log in with wrong credentials', () => {
-    // Test case for invalid login
-  });
-});
