7e4f43a648cf91e5759f51bf9de078a258c56884
Sasha Kasianenko <kasianenkooleksa@gmail.com>
2023-11-30 17:51:02 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -10,25 +10,31 @@ const homePage = new HomePageObject();
 describe('Sign In page', () => {
   let user;
 
-  before(() => {
+  beforeEach(() => {
     cy.task('db:clear');
     cy.task('generateUser').then((generateUser) => {
       user = generateUser;
+      signInPage.visit();
+      cy.register(user.email, user.username, user.password);
     });
   });
 
   it('should provide an ability to log in with existing credentials', () => {
-    signInPage.visit();
-    cy.register(user.email, user.username, user.password);
-
     signInPage.typeEmail(user.email);
     signInPage.typePassword(user.password);
     signInPage.clickSignInBtn();
-
     homePage.assertHeaderContainUsername(user.username);
   });
 
-  it('should not provide an ability to log in with wrong credentials', () => {
+  it('should not provide an ability to log in without email', () => {
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+    signInPage.verifyWrongLogin();
+  });
 
+  it('should not provide an ability to log in without password', () => {
+    signInPage.typeEmail(user.email);
+    signInPage.clickSignInBtn();
+    signInPage.verifyWrongLogin();
   });
 });
