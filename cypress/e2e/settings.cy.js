cb102e76fe6e8c33111b84822ceb46f0289f4c31
Paweł Grzywna <pawelgrzywna01@outlook.com>
2024-01-05 11:30:45 +0100

    Solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,43 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
+import SignInPageObject from '../support/pages/signIn.pageObject';
+import SettingsPageObject from '../support/pages/settings.pageObject';
+
+const signInPage = new SignInPageObject();
+const settingsPage = new SettingsPageObject();
+
 describe('Settings page', () => {
+  let user;
+  let update;
+
   before(() => {
-
-  });
-
-  beforeEach(() => {
-
-  });
-
-  it('should provide an ability to update username', () => {
-
+    cy.visit('/');
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+      cy.register(user.email, user.username, user.password);
+    });
+    cy.task('updateUser').then((updateUser) => {
+      update = updateUser;
+    });
   });
 
   it('should provide an ability to update bio', () => {
-
-  });
-
-  it('should provide an ability to update an email', () => {
-
-  });
-
-  it('should provide an ability to update password', () => {
-
-  });
-
-  it('should provide an ability to log out', () => {
-
+    signInPage.visit();
+    signInPage.emailField.should('exist');
+    signInPage.passwordField.should('exist');
+    signInPage.signInBtn.should('exist');
+    signInPage.typeEmail(user.email);
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+    cy.getByDataCy('username-link').should('contain', user.username);
+    settingsPage.visit();
+    settingsPage.bioTextarea.should('exist');
+    settingsPage.updateSettingsBtn.should('exist');
+    settingsPage.typeBio(update.bio);
+    settingsPage.clickUpdateBtn();
+    cy.get('.swal-modal').should('contain', 'Update successful!');
+    cy.get('.swal-button').click();
   });
 });
