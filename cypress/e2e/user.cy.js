1ed258fd092de2e0ddeebc8fae43e22108c7cfb7
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-01-27 14:35:59 +0200

    user.cy.js

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,12 +1,39 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('User', () => {
-  before(() => {
+import UserPageObject from '../support/pages/user.pageObject';
 
+const userPage = new UserPageObject();
+
+describe('User', () => {
+  let user;
+  let anotherUser;
+
+  before(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+    });
+    cy.task('generateUser').then((generatedUser) => {
+      anotherUser = generatedUser;
+    });
   });
 
-  it.skip('should be able to follow the another user', () => {
+  beforeEach(() => {
+    cy.task('db:clear');
+  });
 
+  it('should be able to follow another user', () => {
+    userPage.visit(anotherUser.username);
+    userPage.followUser();
+
+    userPage.assertUserFollowed(anotherUser.username);
+  });
+
+  it('should be able to unfollow a followed user', () => {
+    userPage.visit(anotherUser.username);
+    userPage.unfollowUser();
+
+    userPage.assertUserUnfollowed(anotherUser.username);
   });
 });
