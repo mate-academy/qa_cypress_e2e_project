2e4843b4c1076694993b4317e4936e8db3faa348
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-01-27 18:57:13 +0200

    signUp.cy.js

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -11,10 +11,13 @@ describe('Sign Up page', () => {
   let user;
 
   before(() => {
-    cy.task('db:clear');
     cy.task('generateUser').then((generatedUser) => {
       user = generatedUser;
     });
+  });
+
+  beforeEach(() => {
+    cy.task('db:clear');
   });
 
   it('should provide an ability to sign up with valid credentials', () => {
