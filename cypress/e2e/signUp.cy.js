c31d5de92d14ec7c1d33e3bd1369bcc09352dab3
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-01-27 14:35:12 +0200

    signUp.cy.js

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,12 +1,39 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Sign Up page', () => {
-  before(() => {
+import SignUpPageObject from '../support/pages/signUp.pageObject';
+import HomePageObject from '../support/pages/home.pageObject';
 
+const signUpPage = new SignUpPageObject();
+const homePage = new HomePageObject();
+
+describe('Sign Up page', () => {
+  let user;
+
+  before(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+    });
   });
 
-  it('should ...', () => {
+  it('should provide an ability to sign up with valid credentials', () => {
+    signUpPage.visit();
+    signUpPage.typeEmail(user.email);
+    signUpPage.typeUsername(user.username);
+    signUpPage.typePassword(user.password);
+    signUpPage.clickSignUpBtn();
 
+    homePage.assertHeaderContainUsername(user.username);
+  });
+
+  it('should not provide an ability to sign up with existing credentials', () => {
+    signUpPage.visit();
+    signUpPage.typeEmail(user.email);
+    signUpPage.typeUsername(user.username);
+    signUpPage.typePassword(user.password);
+    signUpPage.clickSignUpBtn();
+
+    signUpPage.assertSignUpFailed();
   });
 });
