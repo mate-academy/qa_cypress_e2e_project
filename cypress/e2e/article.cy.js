70cc56c9bcd799332b2d2f9654ddd3af33e9a92c
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-01-27 14:31:42 +0200

    article.cy.js

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,9 +1,18 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Article', () => {
-  before(() => {
+import ArticlePageObject from '../support/pages/article.pageObject';
 
+const articlePage = new ArticlePageObject();
+
+describe('Article', () => {
+  let article;
+
+  before(() => {
+    cy.task('db:clear');
+    cy.task('generateArticle').then((generatedArticle) => {
+      article = generatedArticle;
+    });
   });
 
   beforeEach(() => {
@@ -11,14 +20,18 @@ describe('Article', () => {
   });
 
   it('should be created using New Article form', () => {
-
+    articlePage.visit();
+    articlePage.createArticle(article.title, article.description, article.body);
+    articlePage.assertArticleCreated(article.title);
   });
 
   it('should be edited using Edit button', () => {
-
+    articlePage.editArticle(article.title, article.description, article.body);
+    articlePage.assertArticleEdited(article.title);
   });
 
   it('should be deleted using Delete button', () => {
-
+    articlePage.deleteArticle(article.title);
+    articlePage.assertArticleDeleted(article.title);
   });
 });
