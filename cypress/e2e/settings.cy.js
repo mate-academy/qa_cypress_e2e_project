1759b87729d7638f7d981469920e12fcc5c9b51e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-01-27 14:32:52 +0200

    settings.cy.js

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,47 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Settings page', () => {
-  before(() => {
+import SettingsPageObject from '../support/pages/settings.pageObject';
 
+const settingsPage = new SettingsPageObject();
+
+describe('Settings page', () => {
+  let user;
+
+  before(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+    });
   });
 
   beforeEach(() => {
-
+    cy.task('db:clear');
   });
 
   it('should provide an ability to update username', () => {
-
+    settingsPage.visit();
+    settingsPage.updateUsername(user.username);
+    settingsPage.assertUsernameUpdated(user.username);
   });
 
   it('should provide an ability to update bio', () => {
-
+    settingsPage.updateBio(user.bio);
+    settingsPage.assertBioUpdated(user.bio);
   });
 
   it('should provide an ability to update an email', () => {
-
+    settingsPage.updateEmail(user.email);
+    settingsPage.assertEmailUpdated(user.email);
   });
 
   it('should provide an ability to update password', () => {
-
+    settingsPage.updatePassword(user.password);
+    settingsPage.assertPasswordUpdated(user.password);
   });
 
   it('should provide an ability to log out', () => {
-
+    settingsPage.logout();
+    settingsPage.assertLoggedOut();
   });
 });
