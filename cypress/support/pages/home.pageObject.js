7aee189b5cc152467b6a4911fe5c8a1929703366
Maksym Tarasov <tarasovmv90@gmail.com>
2024-01-29 22:54:20 +0200

    змінено:       cypress.config.js 	змінено:       cypress/e2e/article.cy.js 	змінено:       cypress/e2e/settings.cy.js 	змінено:       cypress/e2e/signIn.cy.js 	змінено:       cypress/e2e/signUp.cy.js 	змінено:       cypress/e2e/user.cy.js 	змінено:       cypress/support/PageObject.js 	змінено:       cypress/support/commands.js 	змінено:       cypress/support/index.d.ts 	новий файл:    cypress/support/pages/article.pageObject.js 	змінено:       cypress/support/pages/home.pageObject.js 	новий файл:    cypress/support/pages/settings.pageObject.js 	змінено:       cypress/support/pages/signIn.pageObject.js 	новий файл:    cypress/support/pages/signUp.pageObject.js 	змінено:       src/vue/components/ArticleActions.vue 	змінено:       src/vue/components/Header.vue 	змінено:       src/vue/routes/Article.vue 	змінено:       src/vue/routes/ArticleEdit.vue 	змінено:       src/vue/routes/Login.vue 	змінено:       src/vue/routes/Profile.vue 	змінено:       src/vue/routes/Register.vue 	змінено:       src/vue/routes/Settings.vue

--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -4,12 +4,30 @@ class HomePageObject extends PageObject {
   url = '/#/';
 
   get usernameLink() {
-    return cy.getByDataCy('username-link');
+    return cy.getByDataQa('username-link');
+  }
+
+  get clickableUsername() {
+    return cy.getByDataQa('clickable-username');
+  }
+
+  get followUser() {
+    return cy.getByDataQa('follow-user');
   }
 
   assertHeaderContainUsername(username) {
     this.usernameLink
       .should('contain', username);
+  }
+
+  clickOnUsername() {
+    this.usernameLink
+      .click();
+  }
+
+  assertHeaderNotContainUsername() {
+    this.usernameLink
+      .should('not.exist');
   }
 }
 
