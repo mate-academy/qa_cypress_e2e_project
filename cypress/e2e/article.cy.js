e7319344905494e2256c3df04a4b8efd8871e6ad
Danylo Melentiev <danbigbar@gmail.com>
2024-01-30 17:00:57 +0200

    Cypress practice solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,24 +1,59 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Article', () => {
-  before(() => {
+/// <reference types="cypress" />
+/// <reference types="../support" />
 
-  });
+import EditorPageObject from '../support/pages/editor.pageObject';
+import ArticlePageObject from '../support/pages/article.pageObject';
+import HomePageObject from '../support/pages/home.pageObject';
+
+const editorPage = new EditorPageObject();
+const articlePage = new ArticlePageObject();
+const homePage = new HomePageObject();
+
+describe('Article', () => {
+  let user;
+  let art;
 
   beforeEach(() => {
     cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateArticle').then((generateArticle) => {
+      art = generateArticle;
+    });
   });
 
   it('should be created using New Article form', () => {
-
+    editorPage.login(user.username, user.email, user.password);
+    editorPage.visit();
+    editorPage.typeTitle(art.title);
+    editorPage.typeDescription(art.description);
+    editorPage.typeBody(art.body);
+    editorPage.typeTags(art.tag);
+    editorPage.clickPublishButton();
+    articlePage.assertArticle(art.title, art.body);
   });
 
   it('should be edited using Edit button', () => {
-
+    editorPage.login(user.username, user.email, user.password);
+    editorPage.visit();
+    editorPage.createArt(art.title, art.description, art.body, art.tags);
+    articlePage.clickEdit();
+    editorPage.typeTitle(art.newTitle);
+    editorPage.typeBody(art.newBody);
+    editorPage.clickPublishButton();
+    articlePage.assertArticle(art.newTitle, art.newBody);
   });
 
   it('should be deleted using Delete button', () => {
-
+    editorPage.login(user.username, user.email, user.password);
+    editorPage.visit();
+    editorPage.createArt(art.title, art.description, art.body, art.tags);
+    articlePage.clickDelete();
+    homePage.clickUsername();
+    homePage.assertNoArticles();
   });
 });
--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -7,9 +7,66 @@ class HomePageObject extends PageObject {
     return cy.getByDataCy('username-link');
   }
 
+  get homeLink() {
+    return cy.getByDataCy('home-link');
+  }
+
+  get newArticleLink() {
+    return cy.getByDataCy('newarticle-link');
+  }
+
+  get settingLink() {
+    return cy.getByDataCy('settings-link');
+  }
+
+  get signInLink() {
+    return cy.getByDataCy('login-link');
+  }
+
+  get signUpLink() {
+    return cy.getByDataCy('sign-up-link');
+  }
+
+  clickUsername() {
+    this.usernameLink.click();
+  }
+
+  clickHome() {
+    this.homeLink.click();
+  }
+
+  clickNewArticle() {
+    this.newArticleLink.click();
+  }
+
+  clickSettings() {
+    this.settingLink.click();
+  }
+
+  clickSignIn() {
+    this.signInLink.click();
+  }
+
+  clickSignUp() {
+    this.signUpLink.click();
+  }
+
   assertHeaderContainUsername(username) {
     this.usernameLink
       .should('contain', username);
+  }
+
+  assertHeaderContainSignIn() {
+    this.signInLink
+      .should('contain', 'Sign in');
+  }
+
+  get articlesPrewie() {
+    return cy.get('.article-preview');
+  }
+
+  assertNoArticles() {
+    this.articlesPrewie.should('contain', 'No articles are here... yet.');
   }
 }
 
