9eaaa2bdc90adaaee72a92e19e09e8bd6fa0486d
Jakub Stein <steiniak20@gmail.com>
2024-05-22 10:08:06 +0200

    Settings, userUpdate

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('cypress');
-const faker = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
 const { clear } = require('./server/db');
 const { seed } = require('./server/db');
 const {
@@ -13,9 +13,11 @@ module.exports = defineConfig({
       on('task', {
         generateUser() {
           const randomNumber = Math.ceil(Math.random(1000) * 1000);
+          const userName =
+            faker.name.firstName().toLowerCase() + `${randomNumber}`;
           return {
-            username: faker.name.firstName() + `${randomNumber}`,
-            email: 'test' + `${randomNumber}` + '@mail.com',
+            username: userName,
+            email: userName + '@mail.com',
             password: '12345Qwert!'
           };
         },
--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,61 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Settings page', () => {
-  before(() => {
+import { faker } from '@faker-js/faker';
+import SettingsPageObject from '../support/pages/settings.pageObject';
+import HomePageObject from '../support/pages/home.pageObject';
 
-  });
+const settingsPage = new SettingsPageObject();
+const homePage = new HomePageObject();
+
+describe('Settings page', () => {
+  let user;
 
   beforeEach(() => {
-
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+      cy.register(user.email, user.username, user.password).then(() => {
+        cy.login(user.email, user.password);
+      });
+    });
   });
 
   it('should provide an ability to update username', () => {
-
+    const userName = faker.name.firstName().toLowerCase();
+    settingsPage.visit();
+    settingsPage.typeUsername(userName);
+    settingsPage.clickOnUpdateSettingsButton();
+    settingsPage.assertUpdate();
   });
 
   it('should provide an ability to update bio', () => {
-
+    const bio = faker.lorem.paragraph().toLowerCase();
+    settingsPage.visit();
+    settingsPage.typeBio(bio);
+    settingsPage.clickOnUpdateSettingsButton();
+    settingsPage.assertUpdate();
   });
 
   it('should provide an ability to update an email', () => {
-
+    const email = faker.internet.email().toLowerCase();
+    settingsPage.visit();
+    settingsPage.typeEmail(email);
+    settingsPage.clickOnUpdateSettingsButton();
+    settingsPage.assertUpdate();
   });
 
   it('should provide an ability to update password', () => {
-
+    const password = faker.internet.password();
+    settingsPage.visit();
+    settingsPage.typePassword(password);
+    settingsPage.clickOnUpdateSettingsButton();
+    settingsPage.assertUpdate();
   });
 
   it('should provide an ability to log out', () => {
-
+    settingsPage.visit();
+    settingsPage.clickOnLogoutButton();
+    homePage.usernameLink.should('not.exist');
   });
 });
