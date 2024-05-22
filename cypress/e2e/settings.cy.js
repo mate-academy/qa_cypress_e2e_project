679092a8bd541e2da2ac4c6c8fa3d363e73db34b
Nivon35 <Nivon35@gmail.com>
2024-05-22 15:53:04 +0200

    bio, username, email and password update + logout

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,15 +1,13 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-// import SignInPageObject from '../support/pages/signIn.pageObject';
-// import SignUpPageObject from '../support/pages/signUp.pageObject';
 import SettingsPageObject from '../support/pages/settings.pageObject';
-// import HomePageObject from '../support/pages/home.pageObject';
+import SignInPageObject from '../support/pages/signIn.pageObject';
 
-// const signInPage = new SignInPageObject();
-// const signUpPage = new SignUpPageObject();
+const { faker } = require('@faker-js/faker');
+
 const settings = new SettingsPageObject();
-// const homePage = new HomePageObject();
+const signInPage = new SignInPageObject();
 let user;
 
 describe('Settings page', () => {
@@ -17,36 +15,62 @@ describe('Settings page', () => {
     cy.task('db:clear');
     cy.task('generateUser').then((generatedUser) => {
       user = generatedUser;
-
-      cy.visit('/#/register');
-      settings.typeRegisterUserName(user.username);
-      settings.typeRegisterEmail(user.email);
-      settings.typeRegisterPassword(user.password);
-      settings.clickSignUpBtn();
-      cy.get('.swal-button').click();
-      cy.visit('/#/settings');
+      cy.register(user.email, user.username, user.password).then(() => {
+        cy.login(user.email, user.password);
+      });
+      settings.visit();
     });
   });
 
-  it.only('should provide an ability to update username', () => {
-    settings.typeUserName(user.username);
-    // cy.get('input[data-qa="username-field"]').clear().type(userName);
+  it('should provide an ability to update username', () => {
+    const newUsername = faker.name.firstName().toLowerCase();
+    settings.typeUserName(newUsername);
     settings.clickSignUpBtn();
+
+    settings.assertAlert();
+    settings.clickSwalBtn();
+    settings.userNameField.should('have.value', user.username + newUsername);
   });
 
   it('should provide an ability to update bio', () => {
+    const newBio = faker.lorem.paragraph().toLowerCase();
+    settings.typeBio(newBio);
+    settings.clickSignUpBtn();
 
+    settings.assertAlert();
+    settings.clickSwalBtn();
+    settings.bioField.should('have.value', newBio);
   });
 
   it('should provide an ability to update an email', () => {
+    const newEmail = faker.internet.email().toLowerCase();
+    settings.typeEmail(newEmail);
+    settings.clickSignUpBtn();
 
+    settings.assertAlert();
+    settings.clickSwalBtn();
+    settings.visit();
+    settings.emailField.should('have.value', newEmail);
   });
 
   it('should provide an ability to update password', () => {
+    const newPassword = faker.internet.password();
+    settings.typePassword(newPassword);
+    settings.clickSignUpBtn();
 
+    settings.assertAlert();
+    settings.clickSwalBtn();
+    settings.clickLogoutBtn();
+    signInPage.visit();
+    settings.typeRegisterEmail(user.email);
+    settings.typeRegisterPassword(newPassword);
+    settings.clickSignUpBtn();
+    settings.assertUserName(user.username);
   });
 
   it('should provide an ability to log out', () => {
+    settings.clickLogoutBtn();
 
+    settings.assertNotExistUserName();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -48,3 +48,27 @@ Cypress.Commands.add('register', (email = 'riot@qa.team',
 Cypress.Commands.add('findByPlaceholder', (placeholder, tag = 'input') => {
   cy.get(`${tag}[placeholder="${placeholder}"]`);
 });
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.request('POST', '/users/login', {
+    user: {
+      email,
+      password
+    }
+  }).then((response) => {
+    const user = {
+      username: response.body.user.username,
+      email: response.body.user.email,
+      bio: response.body.user.bio,
+      effectiveImage:
+        'https://static.productionready.io/images/smiley-cyrus.jpg',
+      image: response.body.user.image,
+      token: response.body.user.token,
+      userId: response.body.user.id
+
+    };
+    window.localStorage.setItem('user', JSON.stringify(user));
+    cy.setCookie('drash_sess', response.body.user.token);
+    cy.wrap(response.body.user.id).as('userID');
+  });
+});
