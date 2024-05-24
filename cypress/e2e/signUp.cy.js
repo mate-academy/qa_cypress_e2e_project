7a007a625f6d12f1d19a5c01cd99e7dbcd5aae80
gajdziormax@gmail.com <gajdziormax@gmail.com>
2024-05-24 19:49:17 +0200

    Tests

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -6,67 +6,74 @@ import SignUpPageObject from '../support/pages/signUp.pageObject';
 const SignUppage = new SignUpPageObject();
 
 describe('Sign Up page', () => {
-  before(() => {
+  beforeEach(() => {
     cy.task('db:clear');
   });
 
   it('should allow user to create new account', () => {
     cy.visit('/#/');
-    SignUppage.SignUpLinkbtn();
-    SignUppage.fillUserNameField();
-    SignUppage.fillMailField();
-    SignUppage.fillPasswordField();
+    SignUppage.SignUpbtn();
+    SignUppage.UserNameField();
+    SignUppage.MailField();
+    SignUppage.PasswordField();
     SignUppage.clickSignUpBtn();
+    SignUppage.registrationPassedAssert();
   });
 
   it('should not allow user to create accout with invlid username', () => {
     cy.visit('/#/');
-    SignUppage.SignUpLinkbtn();
-    SignUppage.fillUsernameInvalid();
-    SignUppage.fillMailField();
-    SignUppage.fillPasswordField();
+    SignUppage.SignUpbtn();
+    SignUppage.UsernameInvalid();
+    SignUppage.SecondEmail();
+    SignUppage.PasswordField();
     SignUppage.clickSignUpBtn();
+    SignUppage.registrationFailedAssert();
   });
 
   it('should not allow user to create account with invalid email', () => {
     cy.visit('/#/');
-    SignUppage.SignUpLinkbtn();
-    SignUppage.fillUserNameField();
-    SignUppage.fillMailInvalid();
-    SignUppage.fillPasswordField();
+    SignUppage.SignUpbtn();
+    SignUppage.UserNameField();
+    SignUppage.MailInvalid();
+    SignUppage.PasswordField();
     SignUppage.clickSignUpBtn();
+    SignUppage.registrationFailedAssert();
   });
 
   it('should not allow user to create account with invalid password', () => {
     cy.visit('/#/');
-    SignUppage.SignUpLinkbtn();
-    SignUppage.fillUserNameField();
-    SignUppage.fillMailField();
-    SignUppage.fillPasswordInvalid();
+    SignUppage.SignUpbtn();
+    SignUppage.UserNameField();
+    SignUppage.MailField();
+    SignUppage.PasswordInvalid();
     SignUppage.clickSignUpBtn();
+    SignUppage.registrationFailedAssert();
   });
 
   it('should not allow user to create account without filling password', () => {
     cy.visit('/#/');
-    SignUppage.SignUpLinkbtn();
-    SignUppage.fillUserNameField();
-    SignUppage.fillMailField();
+    SignUppage.SignUpbtn();
+    SignUppage.UserNameField();
+    SignUppage.MailField();
     SignUppage.clickSignUpBtn();
+    SignUppage.registrationFailedAssert();
   });
 
   it('should not allow user to create account without filling Email', () => {
     cy.visit('/#/');
-    SignUppage.SignUpLinkbtn();
-    SignUppage.fillUserNameField();
-    SignUppage.fillPasswordField();
+    SignUppage.SignUpbtn();
+    SignUppage.UserNameField();
+    SignUppage.PasswordField();
     SignUppage.clickSignUpBtn();
+    SignUppage.registrationFailedAssert();
   });
 
   it('should not allow user to create account without filling username', () => {
     cy.visit('/#/');
-    SignUppage.SignUpLinkbtn();
-    SignUppage.fillMailField();
-    SignUppage.fillPasswordField();
+    SignUppage.SignUpbtn();
+    SignUppage.MailField();
+    SignUppage.PasswordField();
     SignUppage.clickSignUpBtn();
+    SignUppage.registrationFailedAssert();
   });
 });
--- cypress/support/pages/signUp.pageObject.js
+++ cypress/support/pages/signUp.pageObject.js
@@ -4,15 +4,27 @@ class SignUpPageObject extends PageObject {
   url = '#/register';
 
   get SignUpLinkbtn() {
-    return cy.getByDataQa('SignUp').click();
+    return cy.getByDataQa('SignUp');
+  }
+
+  SignUpbtn() {
+    this.SignUpLinkbtn.click();
   }
 
   get fillUserNameField() {
-    return cy.getByDataQa('usernamefield').type('validusername123');
+    return cy.getByDataQa('usernamefield');
+  }
+
+  UserNameField() {
+    this.fillUserNameField.type('validusername123');
   }
 
   get fillMailField() {
-    return cy.getByDataQa('emailField').type('validEmail@mail.com');
+    return cy.getByDataQa('emailField');
+  }
+
+  MailField() {
+    this.fillMailField.type('validEmail@mail.com');
   }
 
   clickSignUpBtn() {
@@ -20,20 +32,51 @@ class SignUpPageObject extends PageObject {
   }
 
   get fillPasswordField() {
-    return cy.getByDataQa('passwordField').type('validPassword123');
+    return cy.getByDataQa('passwordField');
+  }
+
+  PasswordField() {
+    this.fillPasswordField.type('validPassword123');
   }
 
   get fillUsernameInvalid() {
-    return cy.getByDataQa('usernamefield')
-      .type('Thisusernameisexactly41charactersleght123');
+    return cy.getByDataQa('usernamefield');
+  }
+
+  UsernameInvalid() {
+    this.fillUsernameInvalid.type('Thisusernameisexactly41charactersleght123');
+  }
+
+  get fillSecondEmail() {
+    return cy.getByDataQa('emailField');
+  }
+
+  SecondEmail() {
+    this.fillSecondEmail.type('secondValidEmail@gmail.com');
   }
 
   get fillMailInvalid() {
-    return cy.getByDataQa('emailField').type('invalidEmail');
+    return cy.getByDataQa('emailField');
+  }
+
+  MailInvalid() {
+    this.fillMailInvalid.type('invalidEmail');
   }
 
   get fillPasswordInvalid() {
-    return cy.getByDataQa('passwordField').type('password');
+    return cy.getByDataQa('passwordField');
+  }
+
+  PasswordInvalid() {
+    this.fillPasswordInvalid.type('password');
+  }
+
+  registrationFailedAssert() {
+    cy.get('.swal-title').should('contain', 'Registration failed!');
+  }
+
+  registrationPassedAssert() {
+    cy.get('.swal-title').should('contain', 'Welcome!');
   }
 }
 export default SignUpPageObject;
