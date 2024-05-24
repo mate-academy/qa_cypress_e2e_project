d16d29e28cab52753a590d82d9809da22584242c
Maria <maria.szynaka@gmail.com>
2024-05-25 00:29:45 +0200

    Article test improved

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -27,15 +27,10 @@ describe('Article page', () => {
 
   beforeEach(() => {
     cy.task('db:clear');
-    cy.register(user.email, user.username, user.password);
-    // cy.visit('/#/register');
-    // cy.findByPlaceholder('Username').type(user.username);
-    // cy.findByPlaceholder('Email').type(user.email);
-    // cy.findByPlaceholder('Password').type(`${user.password}{enter}`);
-    // cy.get(':nth-child(4) > .nav-link').should('contain', user.userName);
   });
 
   it('should be able to create an article', () => {
+    cy.register(user.email, user.username, user.password);
     // cy.login(user.email, user.password);
     articlePage.login(user.email, user.password);
     articlePage.goToNewArticle();
@@ -43,20 +38,6 @@ describe('Article page', () => {
     articlePage.typeAboutField(article.description);
     articlePage.typeArticleBodyField(article.body);
     articlePage.clickPublishArticleBtn();
-    // cy.visit('/#/login');
-    // cy.findByPlaceholder('Email').type(user.email);
-    // cy.findByPlaceholder('Password').type(`${user.password}{enter}`);
-    // cy.on('window:confirm', (str) => {
-    //   expect(str).to.equal('Logging you in... Please wait...');
-    //   return true;
-    // });
-    // cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
-    // cy.contains('.nav-link', 'New Article').click();
-    // cy.findByPlaceholder('Article Title').type(article.title);
-    // cy.findByPlaceholder('What\'s this article about?')
-    //   .type(article.description);
-    // cy.findByPlaceholder('Write your article (in markdown)').type(article.body);
-    // cy.contains('Publish Article').click();
     cy.on('window:confirm', (str) => {
       expect(str).to.equal('Please wait...');
       return true;
@@ -66,10 +47,8 @@ describe('Article page', () => {
     cy.url().should('include', '/articles/');
   });
 
-  it.only('should be able to delete an article', () => {
-    // const response
-    // cy.login(user.email, user.password);
-    // const slug = response.body.article.slug;
+  it.only('should be able to edit an article', () => {
+    cy.register(user.email, user.username, user.password);
     let slug;
     articlePage.login(user.email, user.password);
     cy.on('window:confirm', (str) => {
@@ -89,48 +68,62 @@ describe('Article page', () => {
     cy.url().then((url) => {
       if (url.includes('http://localhost:1667/#/articles/')) {
         const startIndex = url.indexOf('http://localhost:1667/#/articles/') +
-           'http://localhost:1667/#/articles/'.length;
+          'http://localhost:1667/#/articles/'.length;
         const endIndex = url.indexOf('/', startIndex);
         slug = url.substring(startIndex, endIndex);
-        // Wykonaj asercję po znalezieniu sluga
-        cy.url().should('include', slug); // Użyj tylko slug bez prefiksu URL
+        cy.url().should('include', slug);
       } else {
         throw new Error('Brak fragmentu "/articles/" w adresie URL');
       }
     });
-    // .then(() => {
-    // });
+    // articlePage.clickEditArticleBtn();
+    // Żeby eslint pzepuścił zakomnetaowałam ale to potrzebe jest do testu
+    // cy.get('.container > .article-meta > :nth-child(3) > .btn-outline-secondary > span')
+    //   .contains('Edit Article').click();
+    articlePage.typeTitleField(article.title);
+    articlePage.typeAboutField(article.description);
+    articlePage.typeArticleBodyField(article.body);
+    articlePage.clickPublishArticleBtn();
+    const expectedTitle = article.title + article.title;
+    cy.get('h1').should('contain', expectedTitle);
+    const expectedAbout = article.description + article.description;
+    cy.get('p').should('contain', expectedAbout);
+  });
+
+  it('should be able to delete an article', () => {
+    cy.register(user.email, user.username, user.password);
+    let slug;
+    articlePage.login(user.email, user.password);
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal('Logging you in... Please wait...');
+      return true;
+    });
+    articlePage.goToNewArticle();
+    articlePage.typeTitleField(article.title);
+    articlePage.typeAboutField(article.description);
+    articlePage.typeArticleBodyField(article.body);
+    articlePage.clickPublishArticleBtn();
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal('Please wait...');
+      return true;
+    });
+    articlePage.createdActicleTitle.should('contain', article.title);
+    cy.url().then((url) => {
+      if (url.includes('http://localhost:1667/#/articles/')) {
+        const startIndex = url.indexOf('http://localhost:1667/#/articles/') +
+          'http://localhost:1667/#/articles/'.length;
+        const endIndex = url.indexOf('/', startIndex);
+        slug = url.substring(startIndex, endIndex);
+        cy.url().should('include', slug);
+      } else {
+        throw new Error('Brak fragmentu "/articles/" w adresie URL');
+      }
+    });
     cy.get('.article-actions').contains('Delete Article').click();
     cy.on('window:confirm', (str) => {
       expect(str).to.equal('Do you really want to delete it?');
       return true;
     });
-    cy.visit(`/article/${slug}`);
-    cy.reload();// I trired to reload page but it doesn't work.
-    cy.visit(`/article/${slug}`, { failOnStatusCode: false });
-    // cy.get('h2').contains('This page could not be found.').should('exist'); // I tried also this but it doesn't work.
-    cy.contains(`This page could not be found.`).should('exist');
-    //   cy.intercept('POST', '/#/login').as('loginRequest'); // Zakładając, że logowanie wysyła żądanie POST do /api/users/login
-    // cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
-    // cy.wait('@loginRequest').then((interception) => {
-    //   response = interception.response.body;
-    //   authToken = response.user.token;
-    // articlePage.createArticle(article.title, article.description, article.body);
-    // cy.createArticle(article.title, article.description, article.body).then((response) => {
-    //   const slug = response.body.article.slug;
-    //   cy.visit(`/article/${slug}`);
-    //   cy.url().should('include', `/article/${slug}`);
-    //   cy.get('.article-actions').contains('Delete Article').click();
-    //   cy.on('window:confirm', (str) => {
-    //     expect(str).to.equal('Do you really want to delete it?');
-    //     return true;
-    //   });
-    //   cy.visit(`/article/${slug}`);
-    //   cy.reload();// I trired to reload page but it doesn't work.
-    //   cy.visit(`/article/${slug}`, { failOnStatusCode: false });
-    //   // cy.get('h2').contains('This page could not be found.').should('exist'); // I tried also this but it doesn't work.
-    //   cy.contains(`This page could not be found.`).should('exist');
-    // });
   });
 
   afterEach(() => {
