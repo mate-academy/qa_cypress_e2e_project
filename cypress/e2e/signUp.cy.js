c95b1537f053e7c3aba99c64c783002337a264f4
MishaKluch <nimamiha@gmail.com>
2024-08-27 12:17:13 +0300

    E2e tests

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,12 +1,33 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
+import SignUpPageObject from '../support/pages/signUp.page-object';
 
+const signUpPage = new SignUpPageObject();
 describe('Sign Up page', () => {
+  let user;
   before(() => {
-
+    cy.task('db:clear');
+  });
+  beforeEach(() => {
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+    });
+    signUpPage.visit();
   });
 
-  it('should ...', () => {
+  it('should provide an ability to sign up with valid credentials', () => {
+    signUpPage.usernameField.type(user.username);
+    signUpPage.emailField.type(user.email);
+    signUpPage.passwordField.type(user.password);
+    signUpPage.signUpButton.click();
+    cy.get('.swal-title').should('contain', 'Welcome!');
+  });
 
+  it('should not provide an ability to sign up without email', () => {
+    signUpPage.usernameField.type(user.username);
+    signUpPage.emailField.type(user.invalidEmail);
+    signUpPage.passwordField.type(user.password);
+    signUpPage.signUpButton.click();
+    cy.get('.swal-title').should('contain', 'Registration failed!');
   });
 });
