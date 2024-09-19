ba5b4bcc96aeebf5241c95bc7990d551cea9ed4e
Bozhenko Roman <bozhenko091999@gmail.com>
2024-09-19 21:50:09 +0300

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,24 +1,90 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Article', () => {
-  before(() => {
+import { faker } from '@faker-js/faker';
 
+import HomePageObject
+  from '../support/pages/home.pageObject';
+import ArticlePageObject
+  from '../support/pages/article.pageObject';
+import NewArticlePageObject
+  from '../support/pages/nArticle.pageObject';
+import SignInPageObject
+  from '../support/pages/signIn.pageObject';
+
+const homePage = new HomePageObject();
+const articlePage = new ArticlePageObject();
+const nArticlePage = new NewArticlePageObject();
+const signInPage = new SignInPageObject();
+
+describe('Article', () => {
+  let user;
+
+  const article = {
+    title: 'TITLE',
+    about: 'ABOUT',
+    text: 'TEXT',
+    tag: 'TEST'
+  };
+
+  const nArticle = {
+    title: faker.lorem.word(),
+    about: faker.lorem.sentence(),
+    text: faker.lorem.paragraph(),
+    tag: faker.lorem.word()
+  };
+
+  before(() => {
+    cy.task('db:clear');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+      cy.register(user.email, user.username, user.password);
+    });
   });
 
   beforeEach(() => {
-    cy.task('db:clear');
+    signInPage.visit();
+
+    signInPage.typeEmail(user.email);
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+
+    homePage.assertHeaderContainUsername(user.username);
   });
 
   it('should be created using New Article form', () => {
+    homePage.visit();
+    homePage.clickNewArticleLink();
 
+    nArticlePage.typeTitleField(article.title);
+    nArticlePage.typeAboutField(article.about);
+    nArticlePage.typeTextField(article.text);
+    nArticlePage.typeTagField(article.tag);
+
+    nArticlePage.clickPublishBtn();
+
+    articlePage.assertTitle(article.title);
   });
 
   it('should be edited using Edit button', () => {
+    articlePage.visit(`/#/articles/${article.title.toLowerCase()}/`);
+    articlePage.clickEditBtn();
 
+    nArticlePage.typeTitleField(nArticle.title);
+    nArticlePage.typeAboutField(nArticle.about);
+    nArticlePage.typeTextField(nArticle.text);
+    nArticlePage.typeTagField(nArticle.tag);
+    nArticlePage.clickPublishBtn();
+
+    articlePage.assertTitle(nArticle.title);
+    articlePage.assertText(nArticle.text);
   });
 
   it('should be deleted using Delete button', () => {
+    articlePage.visit(`/#/articles/${article.title.toLowerCase()}/`);
+    articlePage.clickDeleteBtn();
 
+    homePage.clickYourFeedLink();
+    homePage.assertArticle('No articles are here... yet.');
   });
 });
--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,12 +1,104 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('User', () => {
-  before(() => {
+import { faker } from '@faker-js/faker';
 
+import HomePageObject
+  from '../support/pages/home.pageObject';
+import ArticlePageObject
+  from '../support/pages/article.pageObject';
+import NewArticlePageObject
+  from '../support/pages/nArticle.pageObject';
+import SignInPageObject
+  from '../support/pages/signIn.pageObject';
+import ProfilePageObject
+  from '../support/pages/profile.pageObject';
+
+const homePage = new HomePageObject();
+const articlePage = new ArticlePageObject();
+const nArticlePage = new NewArticlePageObject();
+const signInPage = new SignInPageObject();
+const profilePage = new ProfilePageObject();
+
+describe('User', () => {
+  let fUser;
+  let user;
+
+  const article = {
+    title: faker.lorem.word(),
+    about: faker.lorem.sentence(),
+    text: faker.lorem.paragraph(),
+    tag: faker.lorem.word()
+  };
+
+  before(() => {
+    cy.task('db:clear');
+
+    cy.task('generateUser').then((generateUser) => {
+      fUser = generateUser;
+    });
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+  });
+
+  it(':basic setup:', () => {
+    cy.register(fUser.email, fUser.username, fUser.password);
+    cy.register(user.email, user.username, user.password);
+
+    signInPage.visit();
+
+    signInPage.typeEmail(fUser.email);
+    signInPage.typePassword(fUser.password);
+    signInPage.clickSignInBtn();
+
+    homePage.assertHeaderContainUsername(fUser.username);
+
+    homePage.clickNewArticleLink();
+
+    nArticlePage.typeTitleField(article.title);
+    nArticlePage.typeAboutField(article.about);
+    nArticlePage.typeTextField(article.text);
+    nArticlePage.typeTagField(article.tag);
+
+    nArticlePage.clickPublishBtn();
+
+    articlePage.assertTitle(article.title);
   });
 
   it.skip('should be able to follow the another user', () => {
+    signInPage.visit();
 
+    signInPage.typeEmail(user.email);
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+
+    homePage.assertHeaderContainUsername(user.username);
+
+    homePage.clickArticlePreview(article.title);
+
+    articlePage.clickAuthorLink();
+
+    profilePage.clickFollowBtn();
+
+    // assert something
+  });
+
+  it.skip('should be able to unfollow the another user', () => {
+    signInPage.visit();
+
+    signInPage.typeEmail(user.email);
+    signInPage.typePassword(user.password);
+    signInPage.clickSignInBtn();
+
+    homePage.assertHeaderContainUsername(user.username);
+
+    homePage.clickArticlePreview(article.title);
+
+    articlePage.clickAuthorLink();
+
+    profilePage.clickUnfollowBtn();
+
+    // assert something
   });
 });
