f5e0bf362c9a65511f55ae13d8306dceb387bde5
Piotr <piotrwandas66@gmail.com>
2024-09-23 17:32:37 +0200

    Add attribute

--- src/vue/routes/Profile.vue
+++ src/vue/routes/Profile.vue
@@ -26,6 +26,7 @@
               </button>
               <button
                 class="btn btn-sm btn-outline-secondary action-btn"
+                data-qa="follow-user-btn"
                 v-if="!profile.following"
                 @click.prevent="follow()"
               >
