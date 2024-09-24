e857d6c4c728eeb11e28dec2210c42680e4a2d70
Klodizaur <klaudia@atomicweb.design>
2024-09-24 12:37:38 +0100

    Fix password generating method

--- cypress/support/generate.js
+++ cypress/support/generate.js
@@ -4,9 +4,18 @@ function generateUser() {
   const randomNumber = Math.random().toString().slice(2, 6);
   const username = faker.internet.userName() + "_" + randomNumber;
   const email = `${username}@mail.com`;
-  const password = "12345Qwert!";
+
+  const password = generatePassword();
 
   return { email, password, username };
+}
+
+function generatePassword() {
+  const lower = faker.random.alpha({ count: 4, casing: 'lower' });
+  const upper = faker.random.alpha({ count: 2, casing: 'upper' });
+  const number = faker.random.numeric(2);
+
+  return faker.helpers.shuffle([lower, upper, number]).join('');
 }
 
 module.exports = { generateUser };
