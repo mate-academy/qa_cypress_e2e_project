800c1523f89701e1716cc3a55fba577483a6e561
Anton Zhyhanovskyi <zhyganovskiy7@gmail.com>
2024-11-01 12:38:04 +0200

    Solution

--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -7,9 +7,50 @@ class HomePageObject extends PageObject {
     return cy.getByDataCy('username-link');
   }
 
+  get authorName() {
+    return cy.getByDataCy('author-name-home-page');
+  }
+
+  get followBtn() {
+    return cy.getByDataCy('follow-btn');
+  }
+
+  get unfollowBtn() {
+    return cy.getByDataCy('unfollow-btn');
+  }
+
+  clickOnProfileLink() {
+    this.usernameLink
+      .click();
+  }
+
+  clickOnAuthor() {
+    this.authorName
+      .click();
+  }
+
+  clickOnFollowBtn() {
+    this.followBtn
+      .click();
+  }
+
+  clickUnfollowBtn() {
+    this.unfollowBtn
+      .click();
+  }
+
   assertHeaderContainUsername(username) {
     this.usernameLink
       .should('contain', username);
+  }
+
+  assertHeaderNotContainUsername(username) {
+    this.usernameLink.should('not.exist', username);
+  }
+
+  assertBtnName(text) {
+    this.followBtn
+      .should('include.text', text);
   }
 }
 
