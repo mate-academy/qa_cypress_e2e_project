3df501f8408ca22fbc385c01a5f6dba4c3f86e1c
Vlad <lukashukvlad534@gmail.com>
2024-11-04 16:14:14 +0200

    practice solution01

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,53 @@
-/// <reference types='cypress' />
-/// <reference types='../support' />
+/// <reference types="cypress" />
+/// <reference types="../support" />
+
+import SettingsPageObject from '../support/pages/settings.pageObject';
+
+const settingsPage = new SettingsPageObject();
 
 describe('Settings page', () => {
-  before(() => {
+  let user;
 
+  before(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
   beforeEach(() => {
-
+    cy.task('db:clear');
+    cy.visit('/');
+    cy.register();
+    cy.login();
+    settingsPage.visit();
   });
 
   it('should provide an ability to update username', () => {
-
+    settingsPage.typeUsername(user.username);
+    settingsPage.submitForm();
+    settingsPage.assertProfilePage(user.username);
   });
 
   it('should provide an ability to update bio', () => {
-
+    settingsPage.typeBio(user.bio);
+    settingsPage.submitForm();
+    settingsPage.assertProfilePage();
   });
 
   it('should provide an ability to update an email', () => {
-
+    settingsPage.typeEmail(user.email);
+    settingsPage.submitForm();
+    settingsPage.assertProfilePage();
   });
 
   it('should provide an ability to update password', () => {
-
+    settingsPage.typePassword(user.password);
+    settingsPage.submitForm();
+    settingsPage.assertProfilePage();
   });
 
   it('should provide an ability to log out', () => {
-
+    settingsPage.logout();
+    cy.url().should('include', '/');
   });
 });
