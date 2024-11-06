2eb1196da1332778d0a47f767478121e4b51aae2
Maxim Berestovyi <maxl.beresto2019@gmail.com>
2024-11-06 14:23:07 +0200

    Add e2e tests for required functionality

--- cypress/support/pages/home.pageObject.js
+++ cypress/support/pages/home.pageObject.js
@@ -1,10 +1,9 @@
 import PageObject from '../PageObject';
-
 class HomePageObject extends PageObject {
   url = '/#/';
 
   get usernameLink() {
-    return cy.getByDataCy('username-link');
+    return this.getByDataCy('username-link');
   }
 
   assertHeaderContainUsername(username) {
